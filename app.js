4056dabc3144c659967d517334ebd13331a51155
ghion79 <guillaume.hallier@gmail.com>
2026-06-22 22:58:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const calendar = new FullCalendar.Calendar(
+        document.getElementById("calendar"),
+        {
+
+            initialView: "dayGridMonth",
+
+            locale: "fr",
+
+            selectable: true,
+
+            editable: true,
+
+            nowIndicator: true,
+
+            headerToolbar: {
+
+                left: "prev,next today",
+
+                center: "title",
+
+                right: "dayGridMonth,timeGridWeek,timeGridDay"
+
+            },
+
+            events: [
+
+                {
+
+                    title: "Bienvenue !",
+
+                    start: new Date()
+
+                }
+
+            ]
+
+        });
+
+    calendar.render();
+
+});
